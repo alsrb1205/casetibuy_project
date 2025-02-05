fa8bfb4203371c7d29c4f5ee0a0ee41a38b1e4d5
alsrb1205 <alsrb1205@gmail.com>
2025-02-05 18:24:56 +0900

    aaa

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from './pages/Layout.jsx';
+import './style/common.css'
+import DetailProduct from './pages/DetailProduct.jsx';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Layout />}>
+                    <Route path='/detail' element={<DetailProduct />}/>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ client/src/pages/DetailProduct.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default function DetailProduct() {
+    return (
+        <div className='content detail-bg'>
+            <div className='product-purchase'>
+                <div className='product-purchase-container'>
+                    <div className='customize'>
+                        <div className='customize-left'>
+                            <img src="/images/1.webp" alt="" />
+                            <img src="/images/2.webp" alt="" />
+                            <img src="/images/2.webp" alt="" />
+                            <img src="/images/2.webp" alt="" />
+                            <img src="/images/2.webp" alt="" />
+                            <img src="/images/2.webp" alt="" />
+                        </div>
+                        <div className='customize-right'>
+                            right
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    );
+}
+
