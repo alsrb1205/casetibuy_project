67e94970f5bb715dfdfa9f00d7aef086fdc4f646
alsrb1205 <alsrb1205@gmail.com>
2025-03-13 22:34:54 +0900

    useorder수정

--- client/src/hooks/useReview.js
+++ client/src/hooks/useReview.js
@@ -2,30 +2,39 @@ import React, { useContext } from 'react';
 import { ReviewContext } from '../context/ReviewContext';
 import axios from 'axios';
 import useOrder from './useOrder';
+import { DetailContext } from '../context/DetailContext';
 
 export default function useReview() {
       const {rating, setRating, comment, setComment} = useContext(ReviewContext);
       const{orderList,getOrderList} = useOrder();
-
+  const{detail} = useContext(DetailContext);
 
     const reviewSubmit = async(e) => {
         e.preventDefault();
-        await getOrderList();
-
-
-
+        const orders= await getOrderList();
+      const memberId = localStorage.getItem("user_id");
+      const pid = detail.pid;
+      const targetOrder = orders.find(
+        order =>
+          order.member_id === memberId &&
+        order.product_id === pid 
+      );
+      
+      if (!targetOrder || !targetOrder.order_id) {
+        alert("해당 상품을 주문한 내역이 없습니다.");
+        return;
+      }
 
         // 여기서 rating과 comment를 백엔드에 전송하거나 로직을 처리한다.
         const sendData = {
-          order_id: orderList.order_Id,       // 주문 번호 (예: 주문 상세 페이지나 주문 context에서 가져옴)
-          pid: orderList.product_id,          // 상품 아이디 (예: detail.pid 등)
-          member_id: orderList.member_id,       // 로그인한 회원의 아이디 (예: localStorage 또는 AuthContext에서 가져옴)
-          color: orderList.color,      // 상품의 색상 정보
-          case: orderList.case,        // 상품의 케이스 타입
+          order_id: targetOrder.order_id,       // 주문 번호 (예: 주문 상세 페이지나 주문 context에서 가져옴)
+          pid: targetOrder.product_id,          // 상품 아이디 (예: detail.pid 등)
+          member_id: targetOrder.member_id,       // 로그인한 회원의 아이디 (예: localStorage 또는 AuthContext에서 가져옴)
+          color: targetOrder.color,      // 상품의 색상 정보
+          case: targetOrder.case_type,        // 상품의 케이스 타입
           rating: rating,          // 평점
           comment: comment         // 리뷰 내용
         };
-      
         axios
         .post('http://localhost:9000/review/new', sendData)
         .then(res => {
