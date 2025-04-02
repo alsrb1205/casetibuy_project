ed44e0474c1dc1e8c5137ff0227d0e8d1e6f9b10
alsrb1205 <alsrb1205@gmail.com>
2025-04-02 11:40:03 +0900

    aa

--- sql/aws-casetibuydb.sql
+++ sql/aws-casetibuydb.sql
@@ -4,9 +4,12 @@ use casetibuy;
 show tables;
 
 select * from information_schema.views where table_schema = 'casetibuy';
-
+select * from casetibuy_order;
+select * from casetibuy_order_detail;
+select * from casetibuy_cart;
 drop view view_cart_list;
 select * from view_cart_list;
+
 create view view_cart_list
 as
 select cc.cid,
@@ -15,6 +18,7 @@ select cc.cid,
        cc.color,
        cc.caseType,
        cc.price,
+       cc.kinds,
        cm.id,
        cp.pid,
        cp.pname,
@@ -23,3 +27,9 @@ select cc.cid,
 			  casetibuy_member cm,
               casetibuy_product cp
 		 where cc.id = cm.id and cc.pid = cp.pid;
+         
+INSERT INTO casetibuy_order (member_id, total_price, payment_method, zipcode, address, detail_address)
+                            VALUES ('test11',1,1,1,1,1);
+                            
+INSERT INTO casetibuy_order_detail (order_id, product_id, product_name, qty, unit_price, kinds, color, case_type, product_image)
+VALUES (22, 1,1,1,1,1,1,1,1);
